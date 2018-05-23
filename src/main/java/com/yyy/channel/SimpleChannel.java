d2832152fae5482cdbf09451cfd0f2c60917f1d7
yyy3000 <yyy123456789>
2018-05-23 16:05:48 +0800

    [变动] 多handle执行顺序

--- src/main/java/com/yyy/channel/SimpleChannel.java
+++ src/main/java/com/yyy/channel/SimpleChannel.java
@@ -15,10 +15,16 @@ public class SimpleChannel extends ChannelInitializer<SocketChannel> {
     protected void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
 
-        // outhandle 必须至少在一个inhandle之前 否则不执行
-        pipeline.addLast(new SimpleOutHandle1());
+        // OutHandle 必须至少在一个InHandle之前 否则不执行
+        // OutHandle 按加载顺序倒序执行
+        // 最后一个OutHandle不需要调用super方法
+        // OutHandle 调用ctx的write时会走下一个 OutHandle 的write方法
         pipeline.addLast(new SimpleOutHandle2());
+        pipeline.addLast(new SimpleOutHandle1());
 
+        // InHandle 按加载顺序顺序执行
+        // 最后一个InHandle不需要调用super方法
+        // InHandle 调用ctx的write时会先走 OutHandle 再继续执行
         pipeline.addLast(new SimpleServerHandle());
         pipeline.addLast(new SimpleInHandle1());
         pipeline.addLast(new SimpleInHandle2());
--- src/main/java/com/yyy/server/handle/SimpleOutHandle2.java
+++ src/main/java/com/yyy/server/handle/SimpleOutHandle2.java
@@ -1,5 +1,6 @@
 package com.yyy.server.handle;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelOutboundHandlerAdapter;
 import io.netty.channel.ChannelPromise;
@@ -13,7 +14,25 @@ public class SimpleOutHandle2 extends ChannelOutboundHandlerAdapter {
     @Override
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
         System.out.println("===SimpleOutHandle2=== write");
-        super.write(ctx, msg, promise);
+
+        ByteBuf result = (ByteBuf) msg;
+        // msg中存储的是ByteBuf类型的数据，把数据读取到byte[]中
+        byte[] result1 = new byte[result.readableBytes()];
+        result.readBytes(result1);
+
+        String message = new String(result1);
+        // 接收并打印客户端的信息
+        System.out.println("SimpleOutHandle1 Server send:" + message);
+
+        // 释放资源，这行很关键
+        result.release();
+
+        message = "SimpleOutHandle1 " + message;
+        // 在当前场景下，发送的数据必须转换成ByteBuf数组
+        ByteBuf encoded = ctx.alloc().buffer(4 * message.length());
+        encoded.writeBytes(message.getBytes());
+        ctx.write(encoded);
+        ctx.flush();
     }
 
     @Override
